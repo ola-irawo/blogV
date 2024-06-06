589240df2cd9722e1dab3d98e4b7af71bc896513
Irawo <timothyaremu63@gmail.com>
2024-06-06 16:15:01 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import AllPosts from "./pages/allPosts/AllPosts";
+import NavBar from "./layouts/nav/NavBar";
+import Contact from "./pages/contact/Contact";
+import PostPage from "./pages/postPage/PostPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes className="App">
+      <Route element={<NavBar />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<AllPosts />} />
+        <Route path="/posts/:postId" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
+import React from "react";
+import { createRoot, hydrateRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css"; // Ensure global CSS is imported
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(
+    rootElement,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+} else {
+  createRoot(rootElement).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+// Measure performance if desired
 reportWebVitals();
